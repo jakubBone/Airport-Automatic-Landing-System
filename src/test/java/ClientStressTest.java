461d6780cb2017e5c09fb71f23fa242456b14902
jakubBone <jakub.bone1990@gmail.com>
2024-10-18 18:42:14 +0200

    test: implement error tracking
    - Implemented basic logs path saving system

--- src/test/java/ClientStressTest.java
+++ src/test/java/ClientStressTest.java
@@ -1,36 +1,47 @@
 import client.PlaneClient;
 
 import java.io.IOException;
+
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ClientStressTest {
+class ClientStressTest {
+    static final Logger logger = Logger.getLogger(ClientStressTest.class.getName());
+
     public static void main(String[] args) throws IOException {
+        LoggerConfigurator.configureLogger("Client_error-log.txt");
+        LoggerConfigurator.configureUncaughtExceptionHandler();
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                System.out.println("Client stopped after 1 hour of operation");
+                logger.info("Client stopped after 1 hour of operation");
                 System.exit(0);
-
             }
-        }, 3600000);  // 1h = 3600000ms
-
+        }, 30000);  // 30s
 
         ExecutorService executorService = Executors.newCachedThreadPool();
 
         while(true) {
-            PlaneClient client = new PlaneClient("localhost", 5000);
-            executorService.execute(client);
             try{
-                Thread.sleep(5000);
-            } catch (InterruptedException ex){
-                Thread.currentThread().interrupt();
+                PlaneClient client = new PlaneClient("localhost", 5000);
+                executorService.execute(client);
+                try{
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex){
+                    logger.log(Level.WARNING, "Client thread interrupted", ex);
+                    Thread.currentThread().interrupt();
+                }
+            } catch (Exception e){
+               logger.log(Level.SEVERE, "An error occurred", e);
             }
         }
-
     }
+
+
 }
--- src/test/java/ServerStressTest.java
+++ src/test/java/ServerStressTest.java
@@ -2,26 +2,32 @@ import server.AirportServer;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ServerStressTest {
+    static final Logger logger = Logger.getLogger(ClientStressTest.class.getName());
     public static void main(String[] args) {
+        LoggerConfigurator.configureLogger("Server_error-log.txt");
+        LoggerConfigurator.configureUncaughtExceptionHandler();
+
         AirportServer airportServer = new AirportServer();
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                System.out.println("Server stopped after 1 hour of operation");
+                logger.info("Server stopped after 1 hour of operation");
                 System.exit(0);
 
             }
-        }, 3600000);  // 1h = 3600000ms
+        }, 10000);  // 10s
 
         try {
             airportServer.startServer(5000);
         } catch (Exception ex) {
-            System.err.println("Failed to start the server: " + ex.getMessage());
+            logger.log(Level.SEVERE, "Failed to start the server:", ex.getMessage());
         }
     }
 }
