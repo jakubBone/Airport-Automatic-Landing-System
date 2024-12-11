5f86279a8922d98cb6bd9280256b02b9524a3047
jakubBone <jakub.bone1990@gmail.com>
2024-12-11 19:02:29 +0100

    refactor: improve readability
    - Added Constant.java for static values storage

---
+++ src/main/java/utills/Constant.java
@@ -0,0 +1,25 @@
+package utills;
+
+public class Constant {
+
+    // Altitude
+    public static int HOLDING_ALTITUDE = 1000;
+    public static int STANDBY_ALTITUDE = 1000;
+    public static int MAX_ALTITUDE = 5000;
+    public static int LAND_ALTITUDE = 700;
+
+
+    // Waypoints
+    public static int DESCENT_WAYPOINTS_NUMBER = 320;
+    public static int LAND_WAYPOINTS_NUMBER = 10;
+
+
+    // Arc
+    public static int DESCENT_RADIUS = 5000;
+    public static int ARC_WAYPOINT_NUMBER = 4;
+
+
+    // Movement
+    public static int HOLDING_STEP = 500;
+    public static double DESCENT_ANGLE_STEP = 360.0 / 80;
+}
