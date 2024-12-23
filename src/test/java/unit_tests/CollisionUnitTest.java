747397f05bc8141152571e66040c6450f4422ada
jakubBone <jakub.bone1990@gmail.com>
2024-12-23 09:56:26 +0100

    test: update CollsionUnitTest
    - Implemented mocking

--- src/test/java/unit_tests/CollisionUnitTest.java
+++ src/test/java/unit_tests/CollisionUnitTest.java
@@ -1,12 +1,14 @@
 package unit_tests;
 
-import controller.CollisionDetector;
 import controller.ControlTower;
 import database.AirportDatabase;
+import database.CollisionDAO;
+import database.PlaneDAO;
 import location.Location;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import plane.Plane;
 
 import java.sql.SQLException;
@@ -14,33 +16,22 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class CollisionUnitTest {
-    AirportDatabase database;
+    AirportDatabase mockDatabase;
+    PlaneDAO mockPlaneDAO;
+    CollisionDAO mockCollisionDao;
     ControlTower controlTower;
 
     @BeforeEach
     void setUp() throws SQLException {
-        this.database = new AirportDatabase();
-        this.controlTower = new ControlTower(database);
-
-    }
-
-    @Test
-    @DisplayName("Should test collision registration if distance between planes equal 10 meters")
-    void tesPlanesCollisionAtNearLocalisation(){
-        Plane plane1 = new Plane("0000");
-        plane1.getNavigator().setLocation(new Location(5000, 5000, 4010));
-        Plane plane2 = new Plane("0000");
-        plane2.getNavigator().setLocation(new Location(5000, 5000, 4000));
-
-        controlTower.registerPlane(plane1);
-        controlTower.registerPlane(plane2);
-
-        controlTower.checkCollision();
-
-        assertTrue(plane1.isDestroyed(), "Plane 1 should be destroyed after collision");
-        assertTrue(plane2.isDestroyed(), "Plane 1 should be destroyed after collision");
+        this.mockDatabase = Mockito.mock(AirportDatabase.class);
+        this.mockPlaneDAO = Mockito.mock(PlaneDAO.class);
+        this.mockCollisionDao = Mockito.mock(CollisionDAO.class);
+        when(mockDatabase.getPLANE_DAO()).thenReturn(mockPlaneDAO);
+        when(mockDatabase.getCOLLISION_DAO()).thenReturn(mockCollisionDao);
+        this.controlTower = new ControlTower(mockDatabase);
     }
 
     @Test
@@ -59,6 +50,25 @@ public class CollisionUnitTest {
         assertTrue(plane1.isDestroyed(), "Plane 1 should be destroyed after collision");
         assertTrue(plane2.isDestroyed(), "Plane 1 should be destroyed after collision");
     }
+
+    @Test
+    @DisplayName("Should test collision registration if distance between planes equal 10 meters")
+    void tesPlanesCollisionAtRiskZone(){
+        Plane plane1 = new Plane("0000");
+        plane1.getNavigator().setLocation(new Location(5000, 5000, 4010));
+        Plane plane2 = new Plane("0000");
+        plane2.getNavigator().setLocation(new Location(5000, 5000, 4000));
+
+        controlTower.registerPlane(plane1);
+        controlTower.registerPlane(plane2);
+
+        controlTower.checkCollision();
+
+        assertTrue(plane1.isDestroyed(), "Plane 1 should be destroyed after collision");
+        assertTrue(plane2.isDestroyed(), "Plane 1 should be destroyed after collision");
+    }
+
+
 
     @Test
     @DisplayName("Should test registration collision avoiding if planes beyond risk zone")
