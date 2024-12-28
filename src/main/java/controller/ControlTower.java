4a5cef14017afb7e931dbbe8ee1ebb4cdc32ec74
jakubBone <jakub.bone1990@gmail.com>
2024-12-28 16:45:37 +0100

    refactor: improve ControlTower readability
    - Implemented lambdas, renamed methods and encapsuleted logic

--- src/main/java/controller/ControlTower.java
+++ src/main/java/controller/ControlTower.java
@@ -7,7 +7,6 @@ import location.Location;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import plane.Plane;
-import utills.Constant;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -16,6 +15,9 @@ import java.util.List;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
+
+import static utills.Constant.HOLDING_ENTRY_ALTITUDE;
 
 @Log4j2
 @Getter
@@ -31,65 +33,32 @@ public class ControlTower {
     }
 
     public void registerPlane(Plane plane) {
-        lock.lock();
-        try {
+        executeWithLock(() -> {
             planes.add(plane);
             database.getPLANE_DAO().registerPlaneInDB(plane);
-        } finally {
-            lock.unlock();
-        }
-    }
-    public boolean isSpaceFull(){
-        lock.lock();
-        try {
-            return planes.size() >= Airport.MAX_CAPACITY;
-        } finally {
-            lock.unlock();
-        }
+        });
     }
 
-    public boolean isAtCollisionRiskZone(Plane plane1) {
-        lock.lock();
-        try {
-            for (Plane plane2 : planes) {
-                for(Location waypoint: plane1.getNavigator().getRiskZoneWaypoints()){
-                    if(waypoint.equals(plane2.getNavigator().getLocation())) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        } finally {
-            lock.unlock();
-        }
+    public boolean isSpaceFull() {
+        return executeWithLock(() -> planes.size() >= Airport.MAX_CAPACITY);
+    }
+
+    public boolean isAtCollisionRiskZone(Plane plane) {
+        return executeWithLock(() -> planes.stream()
+                .anyMatch(otherPlane -> plane.getNavigator().getRiskZoneWaypoints()
+                .contains(otherPlane.getNavigator().getLocation())));
     }
 
     public boolean isRunwayAvailable(Runway runway){
-       lock.lock();
-       try {
-           return runway.isAvailable();
-       } finally {
-          lock.unlock();
-       }
+        return executeWithLock(runway::isAvailable);
     }
 
-    public void assignRunway(Runway runway){
-       lock.lock();
-       try {
-            runway.setAvailable(false);
-       } finally {
-          lock.unlock();
-       }
+    public void assignRunway(Runway runway) {
+        executeWithLock(() -> runway.setAvailable(false));
     }
 
-
-    public void releaseRunway(Runway runway){
-       lock.lock();
-       try {
-            runway.setAvailable(true);
-       } finally {
-          lock.unlock();
-       }
+    public void releaseRunway(Runway runway) {
+        executeWithLock(() -> runway.setAvailable(true));
     }
 
     public void releaseRunwayIfPlaneAtFinalApproach(Plane plane, Runway runway){
@@ -99,65 +68,76 @@ public class ControlTower {
     }
 
     public void removePlaneFromSpace(Plane plane) {
-        lock.lock();
-        try {
-            planes.remove(plane);
-        } finally {
-            lock.unlock();
-        }
+        executeWithLock(() -> planes.remove(plane));
     }
+
     public void checkCollision() {
-        lock.lock();
-        String [] collidedIDs = new String[2];
-        try{
+        executeWithLock(() -> {
             for (int i = 0; i < planes.size(); i++) {
                 Plane plane1 = planes.get(i);
                 for (int j = i + 1; j < planes.size(); j++) {
                     Plane plane2 = planes.get(j);
-
-                    Location loc1 = plane1.getNavigator().getLocation();
-                    Location loc2 = plane2.getNavigator().getLocation();
-                    if (arePlanesToClose(loc1, loc2)) {
-                            collidedIDs[0] = plane1.getFlightNumber();
-                            collidedIDs[1] = plane2.getFlightNumber();
-                            database.getCOLLISION_DAO().registerCollisionToDB(collidedIDs);
-                            planes.get(i).setDestroyed(true);
-                            planes.get(j).setDestroyed(true);
-                        log.info("Collision detected between Plane [{}] and Plane [{}]", plane1.getFlightNumber(), plane2.getFlightNumber());
+                    if (arePlanesToClose(plane1.getNavigator().getLocation(), plane2.getNavigator().getLocation())) {
+                        handleCollision(plane1, plane2);
                     }
                 }
             }
-        } finally {
-            lock.unlock();
-        }
+        });
+    }
+
+    private void handleCollision(Plane plane1, Plane plane2){
+        String[] collidedIDs = {plane1.getFlightNumber(), plane2.getFlightNumber()};
+        database.getCOLLISION_DAO().registerCollisionToDB(collidedIDs);
+        plane1.setDestroyed(true);
+        plane2.setDestroyed(true);
+        log.info("Collision detected between Plane [{}] and Plane [{}]", plane1.getFlightNumber(), plane2.getFlightNumber());
     }
 
     private boolean arePlanesToClose(Location loc1, Location loc2) {
-        double distance = Math.sqrt(Math.pow(loc1.getX() - loc2.getX(), 2)
-                + Math.pow(loc1.getY() - loc2.getY(), 2)
-                + Math.pow(loc1.getAltitude() - loc2.getAltitude(), 2));
+        double distance = Math.sqrt(
+                Math.pow(loc1.getX() - loc2.getX(), 2) +
+                Math.pow(loc1.getY() - loc2.getY(), 2) +
+                Math.pow(loc1.getAltitude() - loc2.getAltitude(), 2)
+        );
         return distance <= 10;
     }
 
     public boolean isPlaneApproachingHoldingAltitude(Plane plane) {
-        return plane.getNavigator().getLocation().getAltitude() == Constant.HOLDING_ENTRY_ALTITUDE;
+        return plane.getNavigator().getLocation().getAltitude() == HOLDING_ENTRY_ALTITUDE;
     }
 
     public boolean hasLandedOnRunway(Plane plane, Runway runway){
-        boolean hasLanded = false;
-        if(plane.getNavigator().getLocation().equals(runway.getLandingPoint())){
-            hasLanded = true;
+        boolean hasLanded = plane.getNavigator().getLocation().equals(runway.getLandingPoint());
+        if (hasLanded) {
             database.getPLANE_DAO().registerLandingInDB(plane);
         }
         return hasLanded;
     }
 
     public Plane getPlaneByFlightNumber(String flightNumber){
-        for(Plane plane: planes){
-            if(flightNumber.equals(plane.getFlightNumber())){
-                return plane;
-            };
+        return executeWithLock(() -> planes.stream()
+                .filter(plane -> flightNumber.equals(plane.getFlightNumber()))
+                .findFirst()
+                .orElse(null));
+    }
+
+
+    // Helper methods for locks management
+    private <T> T executeWithLock(Supplier<T> action){
+        lock.lock();
+        try {
+            return action.get();
+        } finally {
+            lock.unlock();
         }
-        return null;
+    }
+
+    private void executeWithLock(Runnable action){
+        lock.lock();
+        try {
+            action.run();
+        } finally {
+            lock.unlock();
+        }
     }
 }
