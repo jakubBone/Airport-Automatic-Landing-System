bb28c61272b37e2014aedb4102157c2e9c4a36d3
jakubBone <jakub.bone1990@gmail.com>
2024-12-29 17:31:42 +0100

    feat: implement runway factory
    - Created RunwayFactory with simplier runway creation solution

--- src/main/java/airport/Airport.java
+++ src/main/java/airport/Airport.java
@@ -1,9 +1,7 @@
 package airport;
 
-import location.Location;
 import lombok.Getter;
 import utills.Constant;
-
 
 @Getter
 public class Airport {
@@ -12,11 +10,19 @@ public class Airport {
     public static Runway runway1;
 
     public Airport() {
-        this.runway1 = createRunway("R-1", "C-1", Constant.LANDING_POINT_RUNWAY_1, Constant.ENTRY_POINT_CORRIDOR_1, Constant.FINAL_APPROACH_CORRIDOR_1);
-        this.runway2 = createRunway("R-2", "C-2", Constant.LANDING_POINT_RUNWAY_2, Constant.ENTRY_POINT_CORRIDOR_2, Constant.FINAL_APPROACH_CORRIDOR_2);
-    }
-
-    private Runway createRunway(String runwayId, String corridorId, Location landingPoint, Location entryPoint, Location finalApproach) {
-        return new Runway(runwayId, landingPoint, new Corridor(corridorId, entryPoint, finalApproach));
+        this.runway1 = RunwayFactory.create(
+                "R-1",
+                "C-1",
+                Constant.LANDING_POINT_RUNWAY_1,
+                Constant.ENTRY_POINT_CORRIDOR_1,
+                Constant.FINAL_APPROACH_CORRIDOR_1
+        );
+        this.runway2 = RunwayFactory.create(
+                "R-2",
+                "C-2",
+                Constant.LANDING_POINT_RUNWAY_2,
+                Constant.ENTRY_POINT_CORRIDOR_2,
+                Constant.FINAL_APPROACH_CORRIDOR_2
+        );
     }
 }
