aea497fe58dee6d6eb6fb8b4f19cc4c9d27575b3
jakubBone <jakub.bone1990@gmail.com>
2024-12-30 17:53:40 +0100

    refactor: Builder pattern implemented
    - Add @Builder to Runway creating

--- src/main/java/com/jakub/bone/domain/airport/Airport.java
+++ src/main/java/com/jakub/bone/domain/airport/Airport.java
@@ -7,21 +7,18 @@ import com.jakub.bone.utills.Constant;
 public class Airport {
     public static Runway runway2;
     public static Runway runway1;
-
     public Airport() {
-        this.runway1 = RunwayBuilder.create(
-                "R-1",
-                "C-1",
-                Constant.LANDING_POINT_RUNWAY_1,
-                Constant.ENTRY_POINT_CORRIDOR_1,
-                Constant.FINAL_APPROACH_CORRIDOR_1
-        );
-        this.runway2 = RunwayBuilder.create(
-                "R-2",
-                "C-2",
-                Constant.LANDING_POINT_RUNWAY_2,
-                Constant.ENTRY_POINT_CORRIDOR_2,
-                Constant.FINAL_APPROACH_CORRIDOR_2
-        );
+        this.runway1 = Runway.builder()
+                .id("R-1")
+                .landingPoint(Constant.LANDING_POINT_RUNWAY_1)
+                .corridor(new Corridor("C-1", Constant.ENTRY_POINT_CORRIDOR_1, Constant.FINAL_APPROACH_CORRIDOR_1))
+                .available(true)
+                .build();
+        this.runway2 = Runway.builder()
+                .id("R-2")
+                .landingPoint(Constant.LANDING_POINT_RUNWAY_2)
+                .corridor(new Corridor("C-2", Constant.ENTRY_POINT_CORRIDOR_2, Constant.FINAL_APPROACH_CORRIDOR_2))
+                .available(true)
+                .build();
     }
 }
--- src/main/java/com/jakub/bone/domain/airport/Runway.java
+++ src/main/java/com/jakub/bone/domain/airport/Runway.java
@@ -1,5 +1,6 @@
 package com.jakub.bone.domain.airport;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
@@ -9,16 +10,17 @@ import java.io.Serializable;
 @Log4j2
 @Getter
 @Setter
+@Builder
 public class Runway implements Serializable {
     private String id;
     private Location landingPoint;
     private Corridor corridor;
     private boolean available;
 
-    public Runway(String id, Location landingPoint, Corridor corridor) {
+    public Runway(String id, Location landingPoint, Corridor corridor, boolean available) {
         this.id = id;
         this.landingPoint = landingPoint;
         this.corridor = corridor;
-        this.available = false;
+        this.available = available;
     }
 }
